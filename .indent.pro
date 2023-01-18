4b96afaea33a52da483cfba6d3fa62b3c1383cb0
0xLogN <nutdriver716@gmail.com>
2023-01-18 09:10:20 -0800

    add gnu indent

---
+++ .indent.pro
@@ -0,0 +1,6 @@
+-par -bad -bap -bbb -cdb -sc -br -ce -cdw -slc -npcs -nss -ncs -nbc -brs -brf -sar -nut -i4 -lp -ppi4 -bbo -hnl -gts -npsl
+
+-T multiboot_info_t
+-T uint8_t
+-T uint16_t
+-T uint32_t
--- main.c
+++ main.c
@@ -7,7 +7,6 @@
 #include <regs.h>
 #include <ksyms.h>
 
-
 void main(uint32_t multiboot_magic, multiboot_info_t *multiboot) {
     tty_init();
     disable_cursor();
@@ -19,11 +18,13 @@ void main(uint32_t multiboot_magic, multiboot_info_t *multiboot) {
     // make the check that we're using multiboot
     if (multiboot_magic != MULTIBOOT_BOOTLOADER_MAGIC) {
         cpu_registers_t *regs;
+
         init_regs(regs);
-        panic("We were not loaded by a Multiboot-compliant boot-loader!", regs);
+        panic
+            ("We were not loaded by a Multiboot-compliant boot-loader!",
+             regs);
         return;
     }
-
 #ifdef LERNEL_KSYMS_ENABLED
     // TODO: check bit 5 is set (ELF) in multiboot->flags before we do this
     loadksyms(&multiboot->u.elf_sec);
@@ -31,6 +32,7 @@ void main(uint32_t multiboot_magic, multiboot_info_t *multiboot) {
 #endif
 
     cpu_registers_t *regs;
+
     init_regs(regs);
     panic("unimplemented", regs);
 }
