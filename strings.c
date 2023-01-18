c02fca019520364d9fa580045043066023e6e13d
0xLogN <nutdriver716@gmail.com>
2023-01-18 13:02:13 -0800

    feat(strings.c): add some new strings functions

--- strings.c
+++ strings.c
@@ -6,3 +6,35 @@ void strcpy(char *dest, const char *src) {
     }
     *dest = '\0';
 }
+
+unsigned int strlen(const char *s) {
+    unsigned int count = 0;
+
+    while (*s++)
+        count++;
+
+    return count;
+}
+
+void strncpy(char *dest, const char *src, unsigned int n) {
+    while (n-- && *src) {
+        *dest++ = *src++;
+    }
+    *dest = '\0';
+}
+
+int strcmp(const char *s1, const char *s2) {
+    while (*s1 && *s2 && *s1 == *s2) {
+        s1++;
+        s2++;
+    }
+    return *s1 - *s2;
+}
+
+int strncmp(const char *s1, const char *s2, unsigned int n) {
+    while (n-- && *s1 && *s2 && *s1 == *s2) {
+        s1++;
+        s2++;
+    }
+    return *s1 - *s2;
+}
--- strings.h
+++ strings.h
@@ -1,5 +1,7 @@
 #pragma once
 
 void strcpy(char *dest, const char *src);
-
-// TODO: strncpy
+void strncpy(char *dest, const char *src, unsigned int n);
+unsigned int strlen(const char *str);
+int strcmp(const char *str1, const char *str2);
+int strncmp(const char *str1, const char *str2, unsigned int n);
